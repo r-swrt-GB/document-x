6a24b7d07112704eddad1c4388231239a2d7027d
Rikus Swart <rikus.swart@greenbit.dev>
2024-08-25 09:52:35 +0200

    Added vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document X</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-    @vite('resources/js/bootstrap.js')
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
+
 <h1 class="text-3xl font-bold underline">
     Hello world!
 </h1>
