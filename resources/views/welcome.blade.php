a54037e5fb1d64f975d4bec87f22873e4a9a091f
Rikus Swart <rikus.swart@greenbit.dev>
2024-08-25 09:34:33 +0200

    Initial scaffolding with Tailwind and docker added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document X</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    @vite('resources/js/bootstrap.js')
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+</body>
+</html>
